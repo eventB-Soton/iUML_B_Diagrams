23fb9b6846c5dad33c98ce52da2fd93228da73f6
cfsnook <cfs@ecs.soton.ac.uk>
2017-12-15 11:12:45 +0000

    restructured to use new translate all command

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/handlers/IUMLBTranslateAllHandler.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/handlers/IUMLBTranslateAllHandler.java
@@ -12,23 +12,19 @@
 package ac.soton.eventb.emf.diagrams.generator.handlers;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.Diagnostic;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
@@ -39,12 +35,11 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eventb.emf.core.CorePackage;
 import org.eventb.emf.core.EventBElement;
-import org.eventb.emf.core.EventBNamed;
 import org.eventb.emf.persistence.EMFRodinDB;
 import org.rodinp.core.IInternalElement;
 
-import ac.soton.emf.translator.TranslatorFactory;
 import ac.soton.eventb.emf.diagrams.generator.Activator;
+import ac.soton.eventb.emf.diagrams.generator.commands.TranslateAllCommand;
 
 
 /**
@@ -56,7 +51,7 @@ import ac.soton.eventb.emf.diagrams.generator.Activator;
  */
 public class IUMLBTranslateAllHandler extends AbstractHandler {
 
-	final String commandId = "ac.soton.eventb.emf.diagrams.generator.translateToEventB";
+	final static String commandId = "ac.soton.eventb.emf.diagrams.generator.translateToEventB";
 	String report = null;
 	IStatus status = null;
 	
@@ -78,7 +73,7 @@ public class IUMLBTranslateAllHandler extends AbstractHandler {
 		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
 		final Shell shell = activeWorkbenchWindow.getShell();		
 	
-		final EObject component = ((EventBElement)sourceElement).getContaining(CorePackage.Literals.EVENT_BNAMED_COMMENTED_COMPONENT_ELEMENT);
+		final EventBElement component = (EventBElement) ((EventBElement)sourceElement).getContaining(CorePackage.Literals.EVENT_BNAMED_COMMENTED_COMPONENT_ELEMENT);
 		if (component==null ){
 			Activator.getDefault().getLog().log(new Status(Status.WARNING, Activator.PLUGIN_ID, 
 					"Selected element is not a component\n"+
@@ -86,38 +81,20 @@ public class IUMLBTranslateAllHandler extends AbstractHandler {
 			report = "Translation ABORTED - Selected element is not a component";
 			return new Status(IStatus.ERROR, Activator.PLUGIN_ID, report);
 		}else{
-			report = "Translating all iUML-B diagrams in "+((EventBNamed)component).getName()+"\n";
+			report = "";
 		}
 
 		try {
-			//get the diagram elements to be translated
-			final List<EventBElement> generateList = getDiagramRoots(component, null);
-			//get the translator factory
-			final TranslatorFactory factory = TranslatorFactory.getFactory();
 			ProgressMonitorDialog dialog = new ProgressMonitorDialog(shell);
 			dialog.run(true, true, new IRunnableWithProgress(){
 				
 				public void run(IProgressMonitor monitor) throws InvocationTargetException { 
 					try {
-						SubMonitor submonitor = SubMonitor.convert(monitor, "Translating All", 3*generateList.size());								
-						for (EventBElement sourceElement: generateList){
-							if (factory != null && factory.canTranslate(commandId, sourceElement.eClass())){
-								String sourceElementName = sourceElement instanceof EventBNamed?
-										((EventBNamed)sourceElement).getName() :
-										sourceElement.getReference();	
-								EClass sourceClass = sourceElement.eClass();
-								report = report +"\n"+sourceClass.getName()+":"+sourceElementName+" - ";
-								submonitor.setTaskName("Validating "+sourceClass.getName()+" : "+sourceElementName);
-								status = validate(sourceElement, submonitor.newChild(1));
-								if (status.isOK()){
-									submonitor.setTaskName("Translating "+sourceClass.getName()+" : "+sourceElementName);
-									status = factory.translate(sourceElement, commandId, submonitor.newChild(2));
-								}else{
-									submonitor.worked(2);
-								}
-								report = report+status.getMessage();
-							}
-						
+						TransactionalEditingDomain editingDomain = TransactionalEditingDomain.Factory.INSTANCE.getEditingDomain(component.eResource().getResourceSet());
+						TranslateAllCommand translateAllCmd = new TranslateAllCommand(editingDomain,component);
+						if (translateAllCmd.canExecute()){
+							status = translateAllCmd.execute(null, null);
+							report = report+status.getMessage();
 						}
 					} catch (Exception e) {
 						throw new InvocationTargetException(e);
@@ -131,10 +108,6 @@ public class IUMLBTranslateAllHandler extends AbstractHandler {
 		} catch (InterruptedException e) {
 	    	status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.TRANSLATOR_MSG_08, e);        	
 			Activator.logError(Messages.TRANSLATOR_MSG_08, e);
-			report = report + "\n"+e.toString();
-		} catch (CoreException e) {
-	    	status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.TRANSLATOR_MSG_07, e);        	
-			Activator.logError(Messages.TRANSLATOR_MSG_07, e);
 			report = report + "\n"+e.toString();
 		}finally{
 			MessageDialog.openInformation(shell, Messages.TRANSLATOR_MSG_09, report);
@@ -179,20 +152,4 @@ public class IUMLBTranslateAllHandler extends AbstractHandler {
 		}else return null;
 	}
 	
-	/* This recursively traverses the contents tree looking for the root diagram elements that can be generated
-	 * Note that generate-able elements may be nested in others and these should not be generated unless 
-	 * there is an intermediate generate-able element of another type
-	 */
-	private List<EventBElement> getDiagramRoots(EObject element, EClass lastType) throws CoreException {
-		List<EventBElement> generateList = new ArrayList<EventBElement>();
-		if (element instanceof EventBElement && element.eClass()!=lastType && 
-				TranslatorFactory.getFactory().canTranslate(commandId, element.eClass())){
-			generateList.add((EventBElement)element);
-			lastType = element.eClass();
-		}
-		for (EObject eObject : element.eContents()){
-			generateList.addAll(getDiagramRoots(eObject, lastType));
-		}
-		return generateList;
-	}
 }
