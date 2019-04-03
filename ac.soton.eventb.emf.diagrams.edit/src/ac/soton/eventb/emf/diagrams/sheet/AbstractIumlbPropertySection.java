df4a012f8d42a2f366a85d47f34e3caf4bfb83ee
Dr Colin Snook <cfs@ecs.soton.ac.uk>
2019-04-03 14:13:01 +0100

    add base methods for getting the target/components for translation

--- ac.soton.eventb.emf.diagrams.edit/src/ac/soton/eventb/emf/diagrams/sheet/AbstractIumlbPropertySection.java
+++ ac.soton.eventb.emf.diagrams.edit/src/ac/soton/eventb/emf/diagrams/sheet/AbstractIumlbPropertySection.java
@@ -8,18 +8,20 @@
 
 package ac.soton.eventb.emf.diagrams.sheet;
 
-import org.eclipse.emf.common.util.EList;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gmf.runtime.diagram.ui.properties.sections.AbstractModelerPropertySection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eventb.emf.core.Annotation;
-import org.eventb.emf.core.CorePackage;
 import org.eventb.emf.core.EventBElement;
+import org.eventb.emf.core.EventBNamedCommentedComponentElement;
 import org.eventb.emf.core.EventBObject;
+import org.eventb.emf.core.context.Context;
+import org.eventb.emf.core.machine.Machine;
 
 import ac.soton.eventb.emf.diagrams.util.custom.DiagramUtils;
 
@@ -30,8 +32,6 @@ import ac.soton.eventb.emf.diagrams.util.custom.DiagramUtils;
  */
 public abstract class AbstractIumlbPropertySection extends
 		AbstractModelerPropertySection {
-
-	private static final String DIAGRAMS_TRANSLATION_TARGET = "ac.soton.diagrams.translationTarget";
 	
 	/**
 	 * The currently selected EventBElement or the first object in the selection
@@ -94,24 +94,41 @@ public abstract class AbstractIumlbPropertySection extends
 	 * @since 2.4
 	 */
 	protected EventBObject getTranslationTarget() {
-		EventBObject container = owner.getContaining(CorePackage.Literals.PROJECT);
-		if (container == null){
-			container = owner.getContaining(CorePackage.Literals.EVENT_BNAMED_COMMENTED_COMPONENT_ELEMENT);
-		}
-		if (container == null) {
-			EObject root = EcoreUtil.getRootContainer(owner);
-			if (root instanceof EventBElement) {
-				//EventBElement root = getRootElement(owner);
-				Annotation annotation = ((EventBElement)root).getAnnotation(DIAGRAMS_TRANSLATION_TARGET);
-				if (annotation!=null){
-					EList<EObject> references = annotation.getReferences();
-					if (!references.isEmpty() && references.get(0) instanceof EventBObject) {
-						container = (EventBObject) references.get(0);
-					}
-				}
-			}
-		}
-		return container;
+		return DiagramUtils.getTranslationTarget(owner);
 	}
 
+	/**
+	 * This gets all Event-B components that are in scope of the translation target
+	 * i.e. the target itself as well as the closure of all seen or extended contexts 
+	 * 
+	 * @since 2.4
+	 */
+	protected List<EventBNamedCommentedComponentElement> getComponentsInScope() {
+		return getComponentsInScope(getTranslationTarget());
+	}
+	
+	/**
+	 * This gets all Event-B components that are in scope of the one passed as input
+	 * i.e. the input itself as well as the closure of all seen or extended contexts 
+	 * 
+	 * @since 2.4
+	 */
+	protected List<EventBNamedCommentedComponentElement> getComponentsInScope(EventBObject eventBObject) {
+		List<EventBNamedCommentedComponentElement> list =  new ArrayList<EventBNamedCommentedComponentElement>() ;
+		if (eventBObject instanceof Machine){
+			Machine m = ((Machine)eventBObject);
+			list.add(m);
+			for (Context c : m.getSees()){
+				list.addAll(getComponentsInScope(c));
+			}			
+		}else if (eventBObject instanceof Context){
+			Context c = ((Context)eventBObject);
+			list.add(c);
+			for (Context x : c.getExtends()){
+				list.addAll(getComponentsInScope(x));
+			}
+		}
+		return list;
+	}
+	
 }
